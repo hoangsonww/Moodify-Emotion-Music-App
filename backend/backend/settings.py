884ab6eb5ce58863759c3adb53a0c07be0d41fa2
Son Nguyen <hoangson091104@gmail.com>
2024-10-16 20:26:26 -0400

    Update: Add Kubernetes & Jenkins

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,6 +76,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
