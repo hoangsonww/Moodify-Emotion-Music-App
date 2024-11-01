61c73187687b96b9097cc2675dc347a174746ba9
Son Nguyen <hoangson091104@gmail.com>
2024-11-01 12:22:31 -0400

    Update: Add Kubernetes & Jenkins

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -322,7 +322,7 @@ const getStyles = (isDarkMode) => ({
   },
   infoSection: {
     marginBottom: "20px",
-    backgroundColor: isDarkMode ? "#333333" : "#fafafa",
+    backgroundColor: isDarkMode ? "#333333" : "#ffffff",
     padding: "10px",
     borderRadius: "8px",
     boxShadow: "0px 2px 8px rgba(0, 0, 0, 0.1)",
